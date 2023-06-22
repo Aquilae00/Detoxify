87c6fb5123b15682fd9d351b2ded8c2118833901
Brian Linaldi <48035336+Aquilae00@users.noreply.github.com>
2023-06-22 10:07:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ torch.hub.set_dir('/persistent-storage/detoxify')
 model = torch.hub.load('unitaryai/detoxify','unbiased_toxic_roberta')
 from detoxify import Detoxify
 
+model = Detoxify('unbiased',device='cuda')
 class Item(BaseModel):
     text: str
 
 
 def predict(item, run_id, logger):
     item = Item(**item)
-    results = Detoxify('unbiased',device='cuda').predict(item.text)
+    results = model.predict(item.text)
     data = {k: float(v) for k, v in result.items()}
     return data
             
