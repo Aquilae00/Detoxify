8a8e6c63f74f6aaa200d9ca63ecd4c4e0ccf01dc
Brian Linaldi <48035336+Aquilae00@users.noreply.github.com>
2023-06-22 00:01:58 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+import torch
+import os
+torch.hub.set_dir(os.path.join(os.getcwd(), 'persistent-storage', 'detoxify'))
+model = torch.hub.load('unitaryai/detoxify','unbiased_toxic_roberta')
+from detoxify import Detoxify
+
+class Item(BaseModel):
+    text: str
+
+
+def predict(item, run_id, logger):
+    item = Item(**item)
+    results = Detoxify('unbiased').predict(item.text)
+    return results
+            
